993206f62f6ce9099943847c5132106b8057228c
noname <noname@nomail>
2024-03-17 10:52:46 +0100

    Day 4

---
+++ AdventOfCode2022/Day04/CampCleanup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day04;
+static class CampCleanup
+{
+    public static int CountRedundants(string input) => input
+        .Split(Environment.NewLine)
+        .Select(line => line
+            .Split(',', '-')
+            .Select(int.Parse)
+            .ToArray())
+        .Where(pair =>
+        {
+            var (a, b, x, y) = (pair[0], pair[1], pair[2], pair[3]);
+
+            return (a <= x && b >= y) || (x <= a && y >= b);
+        })
+        .Count();
+
+    public static int CountPartiallyRedundants(string input) => input
+        .Split(Environment.NewLine)
+        .Select(line => line
+            .Split(',', '-')
+            .Select(int.Parse)
+            .ToArray())
+        .Where(pair =>
+        {
+            var (a, b, x, y) = (pair[0], pair[1], pair[2], pair[3]);
+
+            return (a <= x && b >= y) || (x <= a && y >= b)
+                || (a <= x && b >= x) || (b >= y && a <= y);
+        })
+        .Count();
+}
---
+++ AdventOfCode2022/Day04/CampCleanupTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Day04;
+
+public sealed class CampCleanupTests
+{
+    [Fact]
+    public void Part1_Test()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day04\\input_test1.txt");
+        var result = CampCleanup.CountRedundants(input);
+
+        result.Should().Be(2);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day04\\input.txt");
+        var result = CampCleanup.CountRedundants(input);
+
+        result.Should().Be(542);
+    }
+
+    [Fact]
+    public void Part2_Test()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day04\\input_test1.txt");
+        var result = CampCleanup.CountPartiallyRedundants(input);
+
+        result.Should().Be(4);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day04\\input.txt");
+        var result = CampCleanup.CountPartiallyRedundants(input);
+
+        result.Should().Be(900);
+    }
+}
