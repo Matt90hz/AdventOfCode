5a4aa3353398760a958791c19597872cc0dfb678
noname <noname@nomail>
2024-03-21 12:04:40 +0100

    Day 8

--- AdventOfCode2022/Day08/TreetopTreeHouse.cs
+++ AdventOfCode2022/Day08/TreetopTreeHouse.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using IncaTechnologies.Collection.Extensions;
+using Xunit;
 
 namespace AdventOfCode2022.Day08;
 static class TreetopTreeHouse
@@ -11,18 +13,25 @@ static class TreetopTreeHouse
     public static int CountTreesVisible(string input) => input
         .Split(Environment.NewLine)
         .ToMultidimensionalArray()
-        .Select(c => c - 48)
+        .Select(c => c - '0')
         .GetPositions()
-        .Select(position => position.IsVisible())
-        .Count(isVisible => isVisible is true);
+        .Count(position => position.IsVisible());
 
-    static bool IsVisible(this IPosition<int> tree) => new[] 
-    { 
-        tree.GetNorth(),
-        tree.GetSouth(),
-        tree.GetWest(),
-        tree.GetEast(),
-    }
-        .Select(otherTrees => otherTrees.All(otherTree => tree.Value > otherTree.Value))
-        .Any(isTallest => isTallest);
+    public static int GetBestViewScore(string input) => input
+        .Split(Environment.NewLine)
+        .ToMultidimensionalArray()
+        .Select(c => c - '0')
+        .GetPositions()
+        .Max(position => position.GetViewScore());
+
+    static int GetViewScore(this IPosition<int> tree) => new[] { tree.GetNorth(), tree.GetSouth(), tree.GetWest(), tree.GetEast() }
+        .Select(otherTrees => otherTrees
+            .TakeWhile(otherTree => otherTree.IsBorder() is false && otherTree.Value < tree.Value)
+            .Count() + 1)
+        .Aggregate((total, score) => total *= score);
+
+    static bool IsVisible(this IPosition<int> tree) => new[] { tree.GetNorth(), tree.GetSouth(), tree.GetWest(), tree.GetEast() }
+        .Any(otherTrees => otherTrees.All(otherTree => otherTree.Value < tree.Value));
 }
+
+
--- AdventOfCode2022/Day08/TreetopTreeHouseTests.cs
+++ AdventOfCode2022/Day08/TreetopTreeHouseTests.cs
@@ -22,4 +22,22 @@ public sealed class TreetopTreeHouseTests
 
         result.Should().Be(1715);
     }
+
+    [Fact]
+    public void Part2_Test1()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day08\\input_test1.txt");
+        var result = TreetopTreeHouse.GetBestViewScore(input);
+
+        result.Should().Be(8);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day08\\input.txt");
+        var result = TreetopTreeHouse.GetBestViewScore(input);
+
+        result.Should().Be(374400);
+    }
 }
