6287efc8528faf5b3c9b9488e7de1e921664a40e
noname <noname@nomail>
2024-03-21 20:30:43 +0100

    Day 9

---
+++ AdventOfCode2022/Day09/RopeBridge.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions.Equivalency.Steps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day09;
+static class RopeBridge
+{
+    public static int CountTailVisitedPositions(string input) => input
+        .Split(Environment.NewLine)
+        .SelectMany(line => Enumerable.Repeat(line[0], int.Parse(line[2..])))
+        .FollowPath()
+        .FollowPath()
+        .Distinct()
+        .Count();
+
+    public static int CountTailVisitedPositionsOfLastKnot(string input) => input
+        .Split(Environment.NewLine)
+        .SelectMany(line => Enumerable.Repeat(line[0], int.Parse(line[2..])))
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .FollowPath()
+        .Distinct()
+        .Count();
+
+    static List<(int Row, int Col)> FollowPath(this IEnumerable<char> steps) => steps
+        .Aggregate(new List<(int Row, int Col)>() { (Row: 0, Col: 0) },
+        (path, step) =>
+        {
+            var last = path.Last();
+
+            var next = step switch
+            {
+                'D' => last with { Row = last.Row + 1 },
+                'U' => last with { Row = last.Row - 1 },
+                'R' => last with { Col = last.Col + 1 },
+                'L' => last with { Col = last.Col - 1 },
+                _ => last
+            };
+
+            path.Add(next);
+
+            return path;
+        });
+
+    static List<(int Row, int Col)> FollowPath(this List<(int Row, int Col)> pathToFollow) => pathToFollow
+        .Aggregate(new List<(int Row, int Col)>() { (Row: 0, Col: 0) },
+        (tailPath, headPos) =>
+        {
+            var tailPos = tailPath.Last();
+
+            (int Row, int Col) nextTailPos = (tailPos.Row - headPos.Row, tailPos.Col - headPos.Col) switch
+            {
+                (-1 or 0 or 1, -1 or 0 or 1) => tailPos,
+                (0, var dCol) => tailPos with { Col = dCol < 0 ? tailPos.Col + 1 : tailPos.Col - 1 },
+                (var dRow, 0) => tailPos with { Row = dRow < 0 ? tailPos.Row + 1 : tailPos.Row - 1 },
+                (var dRow, var dCol) => tailPos with
+                {
+                    Col = dCol < 0 ? tailPos.Col + 1 : tailPos.Col - 1,
+                    Row = dRow < 0 ? tailPos.Row + 1 : tailPos.Row - 1
+                }
+            };
+
+            tailPath.Add(nextTailPos);
+
+            return tailPath;
+        });
+
+}
---
+++ AdventOfCode2022/Day09/RopeBridgeTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Day09;
+
+public sealed class RopeBridgeTests
+{
+    [Fact]
+    public void Part1_Test1()
+    {
+        string input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day09\\input_test1.txt");
+        int result = RopeBridge.CountTailVisitedPositions(input);
+
+        result.Should().Be(13);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        string input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day09\\input.txt");
+        int result = RopeBridge.CountTailVisitedPositions(input);
+
+        result.Should().Be(6498);
+    }
+
+    [Fact]
+    public void Part2_Test1()
+    {
+        string input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day09\\input_test1.txt");
+        int result = RopeBridge.CountTailVisitedPositionsOfLastKnot(input);
+
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void Part2_Test2()
+    {
+        string input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day09\\input_test2.txt");
+        int result = RopeBridge.CountTailVisitedPositionsOfLastKnot(input);
+
+        result.Should().Be(36);
+    }
+    [Fact]
+    public void Part2_Solution()
+    {
+        string input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day09\\input.txt");
+        int result = RopeBridge.CountTailVisitedPositionsOfLastKnot(input);
+
+        result.Should().Be(2531);
+    }
+}
