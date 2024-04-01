11a975250c2f61de86a6023efeb1d2cd9a71dcbb
noname <noname@nomail>
2024-04-01 20:14:17 +0200

    Day 10

---
+++ AdventOfCode2022/Day10/CathodeRayTube.cs
@@ -0,0 +1,42 @@
+﻿using IncaTechnologies.Collection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Day10;
+
+static class CathodeRayTube
+{
+    public static int SumInterestingSignalStranght(string input) => input
+        .Split(Environment.NewLine)
+        .SelectMany<string, int>(line => line == "noop" ? [0] : [0, int.Parse(line[5..])])
+        .Aggregate(new List<int> { 1 },
+        (listOfX, x) =>
+        {
+            listOfX.Add(x + listOfX[^1]);
+
+            return listOfX;
+        })
+        .Select((x, i) => x * (i + 1))
+        .Where((_, i) => i + 1 is 20 or 60 or 100 or 140 or 180 or 220)
+        .Sum();
+
+    public static string GetScreenImage(string input) => input
+        .Split(Environment.NewLine)
+        .SelectMany<string, int>(line => line == "noop" ? [0] : [0, int.Parse(line[5..])])
+        .Aggregate(new List<int> { 1 },
+        (listOfX, x) =>
+        {
+            listOfX.Add(x + listOfX[^1]);
+
+            return listOfX;
+        })
+        .Take(240)
+        .Chunk(40)
+        .Select(cycle => cycle.Select((sprite, pixel) => pixel >= sprite - 1 && pixel <= sprite + 1 ? '#' : '.'))
+        .ToMultidimensionalArray()
+        .ToFriendlyString();
+
+}
---
+++ AdventOfCode2022/Day10/CathodeRayTubeTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Day10;
+
+public sealed class CathodeRayTubeTests
+{
+    [Fact] 
+    public void Part1_Test1()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day10\\input_test1.txt");
+        var result = CathodeRayTube.SumInterestingSignalStranght(input);
+
+        result.Should().Be(13140);
+    }
+
+    [Fact] 
+    public void Part1_Solution() 
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day10\\input.txt");
+        var result = CathodeRayTube.SumInterestingSignalStranght(input);
+
+        result.Should().Be(15020);
+    }
+
+    [Fact]
+    public void Part2_Test1()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day10\\input_test1.txt");
+        var result = CathodeRayTube.GetScreenImage(input);
+
+        result.Should().Be("##..##..##..##..##..##..##..##..##..##.." +
+                       "\r\n###...###...###...###...###...###...###." +
+                       "\r\n####....####....####....####....####...." +
+                       "\r\n#####.....#####.....#####.....#####....." +
+                       "\r\n######......######......######......####" +
+                       "\r\n#######.......#######.......#######.....\r\n");
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day10\\input.txt");
+        var result = CathodeRayTube.GetScreenImage(input);
+
+        result.Should().Be("####.####.#..#..##..#....###...##..###.." +
+                       "\r\n#....#....#..#.#..#.#....#..#.#..#.#..#." +
+                       "\r\n###..###..#..#.#....#....#..#.#..#.#..#." +
+                       "\r\n#....#....#..#.#.##.#....###..####.###.." +
+                       "\r\n#....#....#..#.#..#.#....#....#..#.#...." +
+                       "\r\n####.#.....##...###.####.#....#..#.#....\r\n");
+    }
+}
