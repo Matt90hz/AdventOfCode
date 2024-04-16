dd72489e333441903ec25fa53b311c78ba9da9e4
noname <noname@nomail>
2024-04-16 16:18:30 +0200

    Day 12

---
+++ AdventOfCode2022/Day12/HillClimbingAlgorithm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IncaTechnologies.Collection.Extensions;
+
+namespace AdventOfCode2022.Day12;
+
+static class HillClimbingAlgorithm
+{
+    public static int GetShortestPathToExit(string input)
+    {
+        var map = input
+            .Split(Environment.NewLine)
+            .ToMultidimensionalArray();
+
+        var distances = map.Select(c => c.IsStartPosition() ? 0 : -1);
+
+        Queue<IPosition<char>> positionsToEvaluate = new (map.GetPositions().Where(pos => pos.Value.IsStartPosition()));
+
+        while (positionsToEvaluate.Count > 0)
+        {
+            var positionToEvalute = positionsToEvaluate.Dequeue();
+
+            var nextPositionsToEvaluate = positionToEvalute
+                .GetAdjecents()
+                .Where(adjecent =>
+                    CanClimb(positionToEvalute.Value, adjecent.Value)
+                    && distances[adjecent.Row, adjecent.Column] == -1)
+                .ToArray();
+
+            foreach (var nextPositionToEvaluate in nextPositionsToEvaluate)
+            {
+                distances[nextPositionToEvaluate.Row, nextPositionToEvaluate.Column] = 
+                    distances[positionToEvalute.Row, positionToEvalute.Column] + 1;
+
+                positionsToEvaluate.Enqueue(nextPositionToEvaluate);
+            }
+        }
+
+        var exit = map
+            .GetPositions()
+            .First(position => position.Value.IsBestSignalSpot());
+
+        return distances[exit.Row, exit.Column];
+    }
+
+    public static int GetShortestScenicPath(string input)
+    {
+        var map = input
+            .Split(Environment.NewLine)
+            .ToMultidimensionalArray();
+
+        var distances = map.Select(c => c.IsBestSignalSpot() ? 0 : -1);
+
+        Queue<IPosition<char>> positionsToEvaluate = new(map.GetPositions().Where(pos => pos.Value.IsBestSignalSpot()));
+
+        while (positionsToEvaluate.Count > 0)
+        {
+            var positionToEvalute = positionsToEvaluate.Dequeue();
+
+            var nextPositionsToEvaluate = positionToEvalute
+                .GetAdjecents()
+                .Where(adjecent =>
+                    CanClimbDown(positionToEvalute.Value, adjecent.Value)
+                    && distances[adjecent.Row, adjecent.Column] == -1)
+                .ToArray();
+
+            foreach (var nextPositionToEvaluate in nextPositionsToEvaluate)
+            {
+                distances[nextPositionToEvaluate.Row, nextPositionToEvaluate.Column] =
+                    distances[positionToEvalute.Row, positionToEvalute.Column] + 1;
+
+                positionsToEvaluate.Enqueue(nextPositionToEvaluate);
+            }
+        }
+
+        var starts = map
+            .GetPositions()
+            .Where(position => position.Value == 'a');
+
+        return starts
+            .Select(start => distances[start.Row, start.Column])
+            .Where(distance => distance != -1)
+            .Min();
+    }
+
+    static int GetHeight(this char c) => c switch
+    {
+        'S' => GetHeight('a'),
+        'E' => GetHeight('z'),
+        _ => c - 'a'
+    };
+
+    static bool IsStartPosition(this char c) => c == 'S';
+
+    static bool IsBestSignalSpot(this char c) => c == 'E';
+
+    static bool CanClimb(char from, char to) => (to.GetHeight() - from.GetHeight()) < 2;
+
+    static bool CanClimbDown(char from , char to) => (from.GetHeight() - to.GetHeight()) < 2;
+}
---
+++ AdventOfCode2022/Day12/HillClimbingAlgorithmTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2022.Day12;
+
+public sealed class HillClimbingAlgorithmTests
+{
+    [Fact]
+    public void Part1_Test1()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day12\\input_test1.txt");
+        var result = HillClimbingAlgorithm.GetShortestPathToExit(input);
+
+        result.Should().Be(31);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day12\\input.txt");
+        var result = HillClimbingAlgorithm.GetShortestPathToExit(input);
+
+        result.Should().Be(447);
+    }
+
+    [Fact]
+    public void Part2_Test1()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day12\\input_test1.txt");
+        var result = HillClimbingAlgorithm.GetShortestScenicPath(input);
+
+        result.Should().Be(29);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText("D:\\VisualStudio\\AdventOfCode\\AdventOfCode2022\\Day12\\input.txt");
+        var result = HillClimbingAlgorithm.GetShortestScenicPath(input);
+
+        result.Should().Be(446);
+    }
+}
