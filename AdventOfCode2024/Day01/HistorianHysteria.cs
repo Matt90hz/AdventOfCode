585a4662025b54672449855a4b44d896fa7982d8
noname <noname@nomail>
2024-12-01 11:27:46 +0100

    Solve day 1 2024

---
+++ AdventOfCode2024/Day01/HistorianHysteria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Day01;
+public static class HistorianHysteria
+{
+    public static int TotalDistanceBetweenLists(string input)
+    {
+        var valuePairs = input
+            .Split(Environment.NewLine)
+            .Select(ParseValuePairs);
+
+        List<int> list1 = [];
+        List<int> list2 = [];
+
+        foreach (var (value1, value2) in valuePairs)
+        {
+            list1.Add(value1);
+            list2.Add(value2);
+        }
+
+        list1.Sort();
+        list2.Sort();
+
+        var distances = list1
+            .Zip(list2)
+            .Select(x => Math.Abs(x.First - x.Second));
+
+        var totalDistance = distances.Sum();
+
+        return totalDistance;
+    }
+
+    private static (int X, int Y) ParseValuePairs(string line)
+    {
+        var split = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        var x = int.Parse(split[0]);
+        var y = int.Parse(split[1]);
+
+        return (x, y);
+    }
+}
---
+++ AdventOfCode2024/Day01/HistorianHysteriaTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace AdventOfCode2024.Day01;
+
+public class HistorianHysteriaTests
+{
+    [Fact]
+    public void Part1_Test()
+    {
+        var input = File.ReadAllText(".\\Day01\\input_test.txt");
+        var result = HistorianHysteria.TotalDistanceBetweenLists(input);
+
+        Assert.Equal(11, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(".\\Day01\\input.txt");
+        var result = HistorianHysteria.TotalDistanceBetweenLists(input);
+
+        Assert.Equal(2742123, result);
+    }
+
+    [Fact]
+    public void Part2_Test()
+    {
+        var input = File.ReadAllText(".\\Day01\\input_test.txt");
+        var result = HistorianHysteria.TotalDistanceBetweenLists(input);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(".\\Day01\\input.txt");
+        var result = HistorianHysteria.TotalDistanceBetweenLists(input);
+
+        Assert.Equal(0, result);
+    }
+}
