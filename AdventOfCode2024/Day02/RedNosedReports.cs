1435679d5c9495d9ee7c951c1c433af86ac50be0
noname <noname@nomail>
2024-12-02 13:20:38 +0100

    Solve day 2 aoc 2024

---
+++ AdventOfCode2024/Day02/RedNosedReports.cs
@@ -0,0 +1,51 @@
+﻿namespace AdventOfCode2024.Day02;
+public static class RedNosedReports
+{
+    public static int HowManyReportsAreSafe(string input)
+    {
+        var reports = ParseReports(input);
+        var safe = reports.Where(IsSafe);
+
+        return safe.Count();
+    }
+
+    public static int HowManyReportsAreSafeWithProblemDampener(string input)
+    {
+        var reports = ParseReports(input).Select(x => x.ToArray());
+        var safe = reports.Where(IsSafeWithDampener);
+
+        return safe.Count();
+    }
+
+    private static bool IsSafeWithDampener(int[] report)
+    {
+        var dampenerReports = report
+                .Select((x, i) => report.Where((_, j) => j != i))
+                .Prepend(report);
+
+        var isSafe = dampenerReports.Any(IsSafe);
+
+        return isSafe;
+    }
+
+    private static bool IsSafe(IEnumerable<int> report)
+    {
+        var pairs = report.Zip(report.Skip(1));
+        var differences = pairs.Select(pair => pair.First - pair.Second);
+        int sign = int.Sign(differences.First());
+
+        if(sign == 0) return false;
+
+        var isSafe = differences.All(x => sign == int.Sign(x) && int.Abs(x) <= 3);
+
+        return isSafe;
+    }
+
+    private static IEnumerable<IEnumerable<int>> ParseReports(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var reports = lines.Select(line => line.Split(' ').Select(int.Parse));
+
+        return reports;
+    }
+}
---
+++ AdventOfCode2024/Day02/RedNosedReportsTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace AdventOfCode2024.Day02;
+
+public sealed class RedNosedReportsTests
+{
+    [Fact]
+    public void Part1_Test()
+    {
+        var input = File.ReadAllText(".\\Day02\\input_test.txt");
+        var result = RedNosedReports.HowManyReportsAreSafe(input);
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(".\\Day02\\input.txt");
+        var result = RedNosedReports.HowManyReportsAreSafe(input);
+        Assert.Equal(591, result);
+    }
+
+    [Fact]
+    public void Part2_Test()
+    {
+        var input = File.ReadAllText(".\\Day02\\input_test.txt");
+        var result = RedNosedReports.HowManyReportsAreSafeWithProblemDampener(input);
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(".\\Day02\\input.txt");
+        var result = RedNosedReports.HowManyReportsAreSafeWithProblemDampener(input);
+        Assert.Equal(621, result);
+    }
+}
