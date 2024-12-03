33d474bb227a467e82c0c39f4965e416e4edb38f
noname <noname@nomail>
2024-12-03 12:31:00 +0100

    Solve day 3 part 1 aoc 2024

---
+++ AdventOfCode2024/Day03/MullItOver.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode2024.Day02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Day03;
+public static class MullItOver
+{
+    public static int SumAllMultiplications(string input)
+    {
+        var regex = @"(?<=mul\()\d+,\d+(?=\))";
+
+        var matches = Regex.Matches(input, regex);
+
+        var products = matches.Select(match =>
+        {
+            var values = match.Value
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+
+            var product = values[0] * values[1];
+
+            return product;
+        });
+
+        var result = products.Sum();
+
+        return result;
+    }
+}
---
+++ AdventOfCode2024/Day03/MullItOverTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace AdventOfCode2024.Day03;
+
+public sealed class MullItOverTests
+{
+    [Fact]
+    public void Part1_Test()
+    {
+        var input = File.ReadAllText(".\\Day03\\input_test.txt");
+        var result = MullItOver.SumAllMultiplications(input);
+        Assert.Equal(161, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(".\\Day03\\input.txt");
+        var result = MullItOver.SumAllMultiplications(input);
+        Assert.Equal(170068701, result);
+    }
+
+    [Fact]
+    public void Part2_Test()
+    {
+        var input = File.ReadAllText(".\\Day03\\input_test.txt");
+        var result = MullItOver.SumAllMultiplications(input);
+        Assert.Equal(-1, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(".\\Day03\\input.txt");
+        var result = MullItOver.SumAllMultiplications(input);
+        Assert.Equal(-1, result);
+    }
+}
