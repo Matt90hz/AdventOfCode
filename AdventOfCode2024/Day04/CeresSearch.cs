a17ae3dd040a2b623eb930810d712924622e0d43
noname <noname@nomail>
2024-12-04 11:07:28 +0100

    Solve day 4 part 2 aoc 2024

--- AdventOfCode2024/Day04/CeresSearch.cs
+++ AdventOfCode2024/Day04/CeresSearch.cs
@@ -1,40 +1,71 @@
-﻿namespace AdventOfCode2024.Day04;
+﻿using IncaTechnologies.Collection.Extensions;
+
+namespace AdventOfCode2024.Day04;
 public static class CeresSearch
 {
     public static int CountXMAS(string input)
     {
         char[,] letters = ParseLetters(input);
-        int xmasCount = 0;
+        int count = 0;
 
         for (int i = 0; i < letters.GetLength(0); i++)
         {
             for (int j = 0; j < letters.GetLength(1); j++)
             {
-                xmasCount += Check(letters, i, j, x => x.GetEast());
-                xmasCount += Check(letters, i, j, x => x.GetWest());
-                xmasCount += Check(letters, i, j, x => x.GetNorth());
-                xmasCount += Check(letters, i, j, x => x.GetSouth());
-                xmasCount += Check(letters, i, j, x => x.GetNorthEast());
-                xmasCount += Check(letters, i, j, x => x.GetNorthWest());
-                xmasCount += Check(letters, i, j, x => x.GetSouthEast());
-                xmasCount += Check(letters, i, j, x => x.GetSouthWest());
+                count += CountIfXMAS(letters, i, j, x => x.GetEast());
+                count += CountIfXMAS(letters, i, j, x => x.GetWest());
+                count += CountIfXMAS(letters, i, j, x => x.GetNorth());
+                count += CountIfXMAS(letters, i, j, x => x.GetSouth());
+                count += CountIfXMAS(letters, i, j, x => x.GetNorthEast());
+                count += CountIfXMAS(letters, i, j, x => x.GetNorthWest());
+                count += CountIfXMAS(letters, i, j, x => x.GetSouthEast());
+                count += CountIfXMAS(letters, i, j, x => x.GetSouthWest());
             }
         }
 
-        return xmasCount;
+        return count;
     }
 
-    private static int Check(char[,] letters, int row, int col, Func<IPosition<char>, IEnumerable<IPosition<char>>> direction)
+    public static int CountX_MAS(string input)
+    {
+        char[,] letters = ParseLetters(input);
+
+        int count = 0;
+
+        for (int i = 1; i < letters.GetLength(0) - 1; i++)
+        {
+            for (int j = 1; j < letters.GetLength(1) - 1; j++)
+            {
+                if (letters[i, j] != 'A') continue;
+                
+                var ne = letters[i - 1, j + 1];
+                var nw = letters[i - 1, j - 1];
+                var se = letters[i + 1, j + 1];
+                var sw = letters[i + 1, j - 1]; 
+
+                var is_ne_sw_MAS = (nw == 'M' && se == 'S') || (nw == 'S' && se == 'M');
+                var is_nw_se_MAS = (ne == 'M' && sw == 'S') || (ne == 'S' && sw == 'M');
+
+                var is_x_MAS = is_ne_sw_MAS && is_nw_se_MAS;
+
+                if (is_x_MAS) count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static int CountIfXMAS(char[,] letters, int row, int col, Func<IPosition<char>, IEnumerable<IPosition<char>>> direction)
     {
         if (letters[row, col] != 'X') return 0;
 
         var position = letters.GetPosition(row, col);
 
-        var nextChar = direction(position)
+        var maybeXMAS = direction(position)
             .Take(3)
             .Select(x => x.Value);
 
-        var isXMas = nextChar.SequenceEqual(['M', 'A', 'S']);
+        var isXMas = maybeXMAS.SequenceEqual(['M', 'A', 'S']);
 
         return isXMas ? 1 : 0;
     }
--- AdventOfCode2024/Day04/CeresSearchTests.cs
+++ AdventOfCode2024/Day04/CeresSearchTests.cs
@@ -19,6 +19,24 @@ public sealed class CeresSearchTests
         var input = File.ReadAllText(@".\Day04\input.txt");
         var result = CeresSearch.CountXMAS(input);
 
-        Assert.Equal(0, result);
+        Assert.Equal(2401, result);
+    }
+
+    [Fact]
+    public void Part2_Test()
+    {
+        var input = File.ReadAllText(@".\Day04\input_test.txt");
+        var result = CeresSearch.CountX_MAS(input);
+
+        Assert.Equal(9, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(@".\Day04\input.txt");
+        var result = CeresSearch.CountX_MAS(input);
+
+        Assert.Equal(1822, result);
     }
 }
