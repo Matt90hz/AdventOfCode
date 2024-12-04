ffc3cace623f52a140db9d4fc36670e704f80064
noname <noname@nomail>
2024-12-04 10:22:20 +0100

    Add more look up directions

--- IncaTechnologies.Collection.Extensions/Position.cs
+++ IncaTechnologies.Collection.Extensions/Position.cs
@@ -253,6 +253,38 @@ namespace IncaTechnologies.Collection.Extensions
             }
         }
 
+        public static IEnumerable<IPosition<T>> GetNorthEast<T>(this IPosition<T> position)
+        {
+            for (long i = position.Row - 1, j = position.Column + 1; i >= 0 && j < position.Array.GetLongLength(1); i--, j++)
+            {
+                yield return position.Array.GetPosition(i, j);
+            }
+        }
+
+        public static IEnumerable<IPosition<T>> GetNorthWest<T>(this IPosition<T> position)
+        {
+            for (long i = position.Row - 1, j = position.Column - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                yield return position.Array.GetPosition(i, j);
+            }
+        }
+
+        public static IEnumerable<IPosition<T>> GetSouthEast<T>(this IPosition<T> position)
+        {
+            for (long i = position.Row + 1, j = position.Column + 1; i < position.Array.GetLongLength(0) && j < position.Array.GetLongLength(1); i++, j++)
+            {
+                yield return position.Array.GetPosition(i, j);
+            }
+        }
+
+        public static IEnumerable<IPosition<T>> GetSouthWest<T>(this IPosition<T> position)
+        {
+            for (long i = position.Row + 1, j = position.Column - 1; i < position.Array.GetLongLength(0) && j >= 0; i++, j--)
+            {
+                yield return position.Array.GetPosition(i, j);
+            }
+        }
+
         public static Direction GetLastDirection<T>(this IEnumerable<IPosition<T>> path)
         {
             var last = path.Last();
