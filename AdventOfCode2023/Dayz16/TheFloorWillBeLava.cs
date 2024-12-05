a7557375a3e8b19a43e25682fdddecacc5b73792
noname <noname@nomail>
2024-12-05 17:19:46 +0100

    Optimize day 16 aoc 2023

--- AdventOfCode2023/Dayz16/TheFloorWillBeLava.cs
+++ AdventOfCode2023/Dayz16/TheFloorWillBeLava.cs
@@ -10,21 +10,19 @@ using IncaTechnologies.Collection.Extensions;
 
 namespace AdventOfCode2023.Dayz16;
 
-enum Direction { Left, Right, Up, Down, None }
-
 abstract record ContraptionObject 
 {
     public bool IsEnergized { get; set; } = false;
-
-    public List<Beam> Beams { get; } = new List<Beam>();
 };
 
 record EmptySpace : ContraptionObject;
 record Bound : ContraptionObject;
-record Splitter : ContraptionObject;
-record Mirror : ContraptionObject;
-record FourtyFive : Mirror;
+
+abstract record Mirror : ContraptionObject;
+record FortyFive : Mirror;
 record MinusFortyFive : Mirror;
+
+abstract record Splitter : ContraptionObject;
 record Vertical : Splitter;
 record Horizontal : Splitter;
 
@@ -32,33 +30,33 @@ record Beam(IPosition<ContraptionObject> Position, Direction Direction);
 
 internal static class TheFloorWillBeLava
 {
-
     public static int MaxEnergized(string input)
-    {
+    {      
         var layout = GetLayout(input);
 
-        var topRow = Enumerable.Range(1, layout.GetLength(0) - 2)
-            .Select(i => new Beam(GetLayout(input).GetPosition(1, i), Direction.Down));
+        var topRow = Enumerable
+            .Range(1, layout.GetLength(0) - 2)
+            .Select(i => (Start: (1, i), Direction: Direction.Down));
 
-        var bottomRow = Enumerable.Range(1, layout.GetLength(0) - 2)
-            .Select(i => new Beam(GetLayout(input).GetPosition(layout.GetLength(1) - 2, i), Direction.Up));
+        var bottomRow = Enumerable
+            .Range(1, layout.GetLength(0) - 2)
+            .Select(i => (Start: (layout.GetLength(1) - 2, i), Direction: Direction.Up));
 
-        var leftColumn = Enumerable.Range(1, layout.GetLength(1) - 2)
-            .Select(i => new Beam(GetLayout(input).GetPosition(i, 1), Direction.Right));
+        var leftColumn = Enumerable
+            .Range(1, layout.GetLength(1) - 2)
+            .Select(i => (Start: (i, 1), Direction: Direction.Right));
 
-        var rightColumn = Enumerable.Range(1, layout.GetLength(1) - 2)
-            .Select(i => new Beam(GetLayout(input).GetPosition(i, layout.GetLength(0) - 2), Direction.Left));
+        var rightColumn = Enumerable
+            .Range(1, layout.GetLength(1) - 2)
+            .Select(i => (Start: (i, layout.GetLength(0) - 2), Direction: Direction.Left));
 
-        var beams = new List<Beam>();
+        var energized = topRow
+            .Concat(bottomRow)
+            .Concat(leftColumn)
+            .Concat(rightColumn)
+            .Select(x => Energize(layout, x.Start, x.Direction));
 
-        beams.AddRange(topRow);
-        beams.AddRange(bottomRow);
-        beams.AddRange(leftColumn);
-        beams.AddRange(rightColumn);
-
-        var energizedList = beams.Select(Energized);
-
-        var max = energizedList.Max();
+        var max = energized.Max(x => x.AsEnumerable().Count(x => x.IsEnergized));
 
         return max;
     }
@@ -67,207 +65,87 @@ internal static class TheFloorWillBeLava
     {
         var layout = GetLayout(input);
 
-        var startBeam = new Beam(layout.GetPosition(1, 1), Direction.Right);
+        var energized = Energize(layout, (1, 1), Direction.Right);
 
-        var energized = Energized(startBeam);
+        var count = energized.AsEnumerable().Count(x => x.IsEnergized);
 
-        return energized;
+        return count;
     }
 
-    static int Energized(Beam startBeam)
+    static ContraptionObject[,] Energize(ContraptionObject[,] layout, (int Row, int Col) start, Direction direction)
     {
-        var layout = startBeam.Position.Array;
+        var energized = layout.Select(x => x with { }); //clone
+        var beams = new Stack<Beam>();
 
-        var beams = new List<Beam>() { startBeam };
+        beams.Push(new Beam(energized.GetPosition(start.Row, start.Col), direction));
 
-        layout[startBeam.Position.Row, startBeam.Position.Column].Beams.AddRange(beams);
-        layout[startBeam.Position.Row, startBeam.Position.Column].IsEnergized = true;
-
-        while (beams.Any())
+        while (beams.Count > 0)
         {
-            beams = beams.SelectMany(Move).ToList();
+            var beam = beams.Pop();
 
-            //Task.Delay(5).Wait();
-            //Console.CursorVisible = false;
-            //Console.Clear();
-            //Console.Write(layout.ToFriendlyString(ContraptionObjectToString));
+            var co = beam.Position.Value;
+            var d = beam.Direction;
+            var p = beam.Position;
+
+            switch ((co, d))
+            {
+                case (Bound, _):
+                    continue;
+
+                case (EmptySpace, _) or (Vertical, Direction.Up or Direction.Down) or (Horizontal, Direction.Left or Direction.Right):
+                    beams.Push(beam with { Position = p.Move(d) });
+                    break;
+
+                case (FortyFive, Direction.Up) or (MinusFortyFive, Direction.Down):
+                    beams.Push(beam with { Position = p.MoveRight(), Direction = Direction.Right });
+                    break;
+                case (FortyFive, Direction.Down) or (MinusFortyFive, Direction.Up):
+                    beams.Push(beam with { Position = p.MoveLeft(), Direction = Direction.Left });
+                    break;
+                case (FortyFive, Direction.Left) or (MinusFortyFive, Direction.Right):
+                    beams.Push(beam with { Position = p.MoveDown(), Direction = Direction.Down });
+                    break;
+                case (FortyFive, Direction.Right) or (MinusFortyFive, Direction.Left):
+                    beams.Push(beam with { Position = p.MoveUp(), Direction = Direction.Up });
+                    break;
+
+                case (Vertical, Direction.Left or Direction.Right):
+                    if (co.IsEnergized) continue;
+                    beams.Push(beam with { Position = p.MoveUp(), Direction = Direction.Up });
+                    beams.Push(beam with { Position = p.MoveDown(), Direction = Direction.Down });
+                    break;
+
+                case (Horizontal, Direction.Up or Direction.Down):
+                    if (co.IsEnergized) continue;
+                    beams.Push(beam with { Position = p.MoveLeft(), Direction = Direction.Left });
+                    beams.Push(beam with { Position = p.MoveRight(), Direction = Direction.Right });
+                    break;
+            }
+
+            co.IsEnergized = true;
         }
 
-        var energized = layout
-            .GetRows()
-            .Select(row => row.Count(x => x.IsEnergized))
-            .Sum();
-
         return energized;
     }
 
-    static IEnumerable<Beam> Move(Beam beam)
+    static ContraptionObject[,] GetLayout(string input)
     {
-        var nextPosition = beam.Position.Value switch
+        var lines = input.Split(Environment.NewLine);
+
+        var contraptionObjects = lines.Select(x => x.Select<char, ContraptionObject>(x => x switch
         {
-            FourtyFive => beam.Direction switch
-            {
-                Direction.Left => new[] { beam.MoveBeamDown() },
-                Direction.Right => new[] { beam.MoveBeamUp() },
-                Direction.Up => new[] { beam.MoveBeamRight() },
-                Direction.Down => new[] { beam.MoveBeamLeft() },
-                _ => throw new NotImplementedException(),
-            },
-            MinusFortyFive => beam.Direction switch
-            {
-                Direction.Left => new[] { beam.MoveBeamUp() },
-                Direction.Right => new[] { beam.MoveBeamDown() },
-                Direction.Up => new[] { beam.MoveBeamLeft() },
-                Direction.Down => new[] { beam.MoveBeamRight() },
-                _ => throw new NotImplementedException(),
-            },
-            Vertical => beam.Direction switch
-            {
-                Direction.Left or Direction.Right => new[] { beam.MoveBeamDown(), beam.MoveBeamUp() },
-                Direction.Up or Direction.Down => beam.Direction switch
-                {
-                    Direction.Up => new[] { beam.MoveBeamUp() },
-                    Direction.Down => new[] { beam.MoveBeamDown() },
-                    _ => throw new NotImplementedException(),
-                },
-                _ => throw new NotImplementedException(),
-            },
-            Horizontal => beam.Direction switch
-            {
-                Direction.Up or Direction.Down => new[] { beam.MoveBeamLeft(), beam.MoveBeamRight() },
-                Direction.Left or Direction.Right => beam.Direction switch
-                {
-                    Direction.Left => new[] { beam.MoveBeamLeft() },
-                    Direction.Right => new[] { beam.MoveBeamRight() },
-                    _ => throw new NotImplementedException(),
-                },
-                _ => throw new NotImplementedException(),
-            },
-            _ => beam.Direction switch
-            {
-                Direction.Left => new[] { beam.MoveBeamLeft() },
-                Direction.Right => new[] { beam.MoveBeamRight() },
-                Direction.Up => new[] { beam.MoveBeamUp() },
-                Direction.Down => new[] { beam.MoveBeamDown() },
-                _ => throw new NotImplementedException(),
-            },
-        };
-
-        nextPosition = nextPosition.Where(x => x is not null).ToArray();
-
-        return nextPosition!;
-    }
-
-    static ContraptionObject GetContraptionObject(char x) => x switch
-    {
-        '@' => new Bound(),
-        '|' => new Vertical(),
-        '-' => new Horizontal(),
-        '/' => new FourtyFive(),
-        '\\' => new MinusFortyFive(),
-        _ => new EmptySpace()
-    };
-
-    static ContraptionObject[,] GetLayout(string x)
-    {
-        var layout = x
-            .Split(Environment.NewLine)
-            .Select(x => x.Select(x => GetContraptionObject(x)))
+            '@' => new Bound(),
+            '|' => new Vertical(),
+            '-' => new Horizontal(),
+            '/' => new FortyFive(),
+            '\\' => new MinusFortyFive(),
+            _ => new EmptySpace()
+        }));
+        
+        var layout  = contraptionObjects
             .ToMultidimensionalArray()
             .SurroundWith(new Bound());
 
         return layout;
     }
-
-    static bool IsMovementAllowed(Beam curr, IPosition<ContraptionObject> next)
-    {
-        if (next.Value is Bound) return false;
-
-        if (next.Value.IsEnergized
-            && curr.Position.Value is EmptySpace
-            && next.Value.Beams.Any(x => x.Direction == curr.Direction)) return false;
-
-        return true;
-    }
-
-    static Beam? MoveBeamUp(this Beam beam)
-    {
-        var next = beam.Position.MoveUp();
-
-        if (IsMovementAllowed(beam, next) is false) return null;
-      
-        next.Value.IsEnergized = true;
-
-        var nextBeam = new Beam(next, Direction.Up);
-
-        next.Value.Beams.Add(nextBeam);
-
-        return nextBeam;
-    }
-
-    static Beam? MoveBeamDown(this Beam beam)
-    {
-        var next = beam.Position.MoveDown();
-
-        if (IsMovementAllowed(beam, next) is false) return null;
-
-        next.Value.IsEnergized = true;
-
-        var nextBeam = new Beam(next, Direction.Down);
-
-        next.Value.Beams.Add(nextBeam);
-
-        return nextBeam;
-    }
-
-    static Beam? MoveBeamLeft(this Beam beam)
-    {
-        var next = beam.Position.MoveLeft();
-
-        if (IsMovementAllowed(beam, next) is false) return null;
-
-        next.Value.IsEnergized = true;
-
-        var nextBeam = new Beam(next, Direction.Left);
-
-        next.Value.Beams.Add(nextBeam);
-
-        return nextBeam;
-    }
-
-    static Beam? MoveBeamRight(this Beam beam)
-    {
-        var next = beam.Position.MoveRight();
-
-        if (IsMovementAllowed(beam, next) is false) return null;
-
-        next.Value.IsEnergized = true;
-
-        var nextBeam = new Beam(next, Direction.Right);
-
-        next.Value.Beams.Add(nextBeam);
-
-        return nextBeam;
-    }
-
-    static string ContraptionObjectToString(this ContraptionObject co) => (co, co.IsEnergized) switch
-    {
-        (Bound, _) => "@",
-        (FourtyFive, _) => "/",
-        (MinusFortyFive, _) => "\\",
-        (Vertical, _) => "|",
-        (Horizontal, _) => "-",
-        (EmptySpace, true) => "X",
-        (EmptySpace, false) => ".",    
-        _ => string.Empty,
-    };
 }
-
-//co.Direction switch
-//        {
-//            Direction.Left => "<",
-//            Direction.Right => ">",
-//            Direction.Up => "A",
-//            Direction.Down => "V",
-//            _ => "X",
-//        }
