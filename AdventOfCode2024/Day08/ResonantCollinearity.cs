ec269be7bed407e194c212909de3ff746d2f7613
noname <noname@nomail>
2024-12-08 12:51:04 +0100

    Solve part 2 day 8 aoc 2024

--- AdventOfCode2024/Day08/ResonantCollinearity.cs
+++ AdventOfCode2024/Day08/ResonantCollinearity.cs
@@ -1,7 +1,4 @@
-﻿
-using AdventOfCode2024.Day07;
-
-namespace AdventOfCode2024.Day08;
+﻿namespace AdventOfCode2024.Day08;
 public static class ResonantCollinearity
 {
     public static int CountAntinodes(string input)
@@ -10,6 +7,64 @@ public static class ResonantCollinearity
         var antinodes = FindAntinodes(map);
         var count = antinodes.Count();
         return count;
+    }
+
+    public static int CountResonantAntinodes(string input)
+    {
+        var map = ParseMap(input);
+        var antinodes = FindResonantAntinodes(map);
+        var count = antinodes.Count();
+        return count;
+    }
+
+    private static IEnumerable<Position<char>> FindResonantAntinodes(char[,] map)
+    {
+        var antennas = map
+            .GetPositions()
+            .Where(x => x.Value != '.')
+            .ToArray();
+
+        var antennasPairs = antennas
+            .SelectMany((x, i) => antennas.Skip(i + 1).Select(y => (x, y)))
+            .Where(pair => pair.x.Value == pair.y.Value);
+
+        var antinodes = antennasPairs
+            .SelectMany(pair => CalculateResonantAntinodes(pair.x, pair.y))
+            .Distinct();
+
+        return antinodes;
+    }
+
+    private static IEnumerable<Position<char>> CalculateResonantAntinodes(
+        Position<char> x, 
+        Position<char> y)
+    {
+        var (map, xr, xc) = x;
+        var (yr, yc) = y;
+        var dr = xr - yr;
+        var dc = xc - yc;
+        int i = 1;
+
+        while (true)
+        {
+            var a1 = map.GetPosition(xr + dr * i, xc + dc * i);
+            var a2 = map.GetPosition(yr - dr * i, yc - dc * i);
+
+            bool a1Out = a1.IsOutOfBound();
+            bool a2Out = a2.IsOutOfBound();
+
+            if (a1Out && a2Out)
+            {               
+                yield return x;
+                yield return y;
+                yield break; 
+            }
+
+            if (!a1Out) yield return a1;
+            if (!a2Out) yield return a2;
+
+            i++;
+        }
     }
 
     private static IEnumerable<Position<char>> FindAntinodes(char[,] map)
--- AdventOfCode2024/Day08/ResonantCollinearityTests.cs
+++ AdventOfCode2024/Day08/ResonantCollinearityTests.cs
@@ -24,15 +24,15 @@ public sealed class ResonantCollinearityTests
     public void Part2_Test()
     {
         var input = File.ReadAllText(@".\Day08\input_test.txt");
-        var result = ResonantCollinearity.CountAntinodes(input);
-        Assert.Equal(0, result);
+        var result = ResonantCollinearity.CountResonantAntinodes(input);
+        Assert.Equal(34, result);
     }
 
     [Fact]
     public void Part2_Solution()
     {
         var input = File.ReadAllText(@".\Day08\input.txt");
-        var result = ResonantCollinearity.CountAntinodes(input);
-        Assert.Equal(0, result);
+        var result = ResonantCollinearity.CountResonantAntinodes(input);
+        Assert.Equal(813, result);
     }
 }
