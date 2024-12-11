a36f8fe37b99322c6fc7b225eb0fe9955b67a4f6
noname <noname@nomail>
2024-12-11 12:19:19 +0100

    Solve day 11 aoc 2024

---
+++ AdventOfCode2024/Day11/PlutonianPebbles.cs
@@ -0,0 +1,47 @@
+﻿using AdventOfCode2024.Day10;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace AdventOfCode2024.Day11;
+public static class PlutonianPebbles
+{
+    public static long StonesAfterBlinks(string input, int blinks)
+    {
+        var stones = input
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse);
+
+        var count = stones.Select(x => StonesAfterBlinks(x, blinks, [])).Sum();
+
+        return count;
+    }
+
+    public static long StonesAfterBlinks(long stone, int blinks, Dictionary<(long, long), long> cache)
+    {
+        var key = (stone, blinks); 
+
+        if (cache.ContainsKey(key)) return cache[key];
+
+        if (blinks == 0) return 1;
+
+        if (stone == 0) return StonesAfterBlinks(1, blinks - 1, cache);
+
+        int nDigits = (int)Math.Log10(stone) + 1;
+
+        long stones;
+
+        if (nDigits % 2 == 0) 
+        { 
+            int power = (int)Math.Pow(10, nDigits / 2);
+            var a = StonesAfterBlinks(stone / power, blinks - 1, cache);
+            var b = StonesAfterBlinks(stone % power, blinks - 1, cache);
+            stones = a + b;
+            cache[key] = stones; 
+            return stones; 
+        }
+
+        stones = StonesAfterBlinks(stone * 2024, blinks - 1, cache); 
+        cache[key] = stones; 
+        return stones;
+    }
+}
---
+++ AdventOfCode2024/Day11/PlutonianPebblesTests.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2024.Day11;
+
+public sealed class PlutonianPebblesTests
+{
+    [Fact]
+    public void Part1_Test1()
+    {
+        var input = File.ReadAllText(@".\Day11\input_test.txt");
+        var result = PlutonianPebbles.StonesAfterBlinks("125 17", 6);
+        Assert.Equal(22, result);
+    }
+
+    [Fact]
+    public void Part1_Test2()
+    {
+        var input = File.ReadAllText(@".\Day11\input_test.txt");
+        var result = PlutonianPebbles.StonesAfterBlinks(input, 25);
+        Assert.Equal(55312, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(@".\Day11\input.txt");
+        var result = PlutonianPebbles.StonesAfterBlinks(input, 25);
+        Assert.Equal(207683, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(@".\Day11\input.txt");
+        var result = PlutonianPebbles.StonesAfterBlinks(input, 75);
+        Assert.Equal(244782991106220, result);
+    }
+}
