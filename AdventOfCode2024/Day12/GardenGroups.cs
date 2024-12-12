547e44efa2d3235826935bffd4fceb44969b7132
noname <noname@nomail>
2024-12-12 13:03:50 +0100

    Solve day12 aoc 2024

---
+++ AdventOfCode2024/Day12/GardenGroups.cs
@@ -0,0 +1,100 @@
+﻿using AdventOfCode2024.Day12;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2024.Day12;
+public static class GardenGroups
+{
+    public static int FencingCost(string input)
+    {
+        var plot = ParsePlot(input);
+        var regions = plot.GetRegions();
+        var cost = regions.Sum(x => x.GetArea() * x.GetPerimeter());
+        return cost;
+    }
+
+    public static int FencingDiscountCost(string input)
+    {
+        var plot = ParsePlot(input);
+        var regions = plot.GetRegions();
+        var cost = regions.Sum(x => x.GetArea() * x.GetSides());
+        return cost;
+    }
+
+    private static int GetSides(this IEnumerable<Position<char>> region)
+    {
+        var border = region.Where(x => x.GetAdjacent().Any(y => x.Value))
+    }
+
+    private static int GetPerimeter(this IEnumerable<Position<char>> region)
+    {
+        var sides = region.Select(x =>
+        {
+            var adjacent = x
+                .GetAdjacent()
+                .Where(y => x.Value == y.Value);
+
+            var sides = 4 - adjacent.Count();
+
+            return sides;
+        });
+
+        var perimeter = sides.Sum();
+
+        return perimeter;
+    }
+
+    private static int GetArea(this IEnumerable<Position<char>> region)
+    {
+        var area = region.Count();
+
+        return area;
+    }
+
+    private static IEnumerable<IEnumerable<Position<char>>> GetRegions(this char[,] plot)
+    {
+        var done = plot.Select(_ => false);
+
+        var positions = plot.GetPositions();
+
+        foreach (var position in positions) 
+        {
+            var (row, col) = position;
+
+            if (done[row, col]) continue;
+            
+            var region = GetRegion(position, done);
+
+            yield return region;
+        }
+
+        static IEnumerable<Position<char>> GetRegion(Position<char> x, bool[,] done)
+        {
+            var (r, c) = x;
+
+            done[r, c] = true;
+
+            var adjacent = x
+                .GetAdjacent()
+                .Where(a => a.Value == x.Value && !done[a.Row, a.Column]);
+
+            var region = new List<Position<char>>() { x };
+
+            foreach(var a in adjacent)
+            {
+                region.AddRange(GetRegion(a, done));
+            }
+
+            return region;
+        }
+    }
+
+    private static char[,] ParsePlot(string input)
+    {
+        var plot = input
+            .Split(Environment.NewLine)
+            .Select(x => x.ToCharArray())
+            .ToMultidimensionalArray();
+
+        return plot;
+    }
+}
---
+++ AdventOfCode2024/Day12/GardenGroupsTests.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2024.Day12;
+
+public sealed class GardenGroupsTests
+{
+    [Fact]
+    public void Part1_Test1()
+    {
+        var input = File.ReadAllText(@".\Day12\input_test1.txt");
+        var result = GardenGroups.FencingCost(input);
+        Assert.Equal(140, result);
+    }
+
+    [Fact]
+    public void Part1_Test2()
+    {
+        var input = File.ReadAllText(@".\Day12\input_test2.txt");
+        var result = GardenGroups.FencingCost(input);
+        Assert.Equal(772, result);
+    }
+
+    [Fact]
+    public void Part1_Test3()
+    {
+        var input = File.ReadAllText(@".\Day12\input_test3.txt");
+        var result = GardenGroups.FencingCost(input);
+        Assert.Equal(1930, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(@".\Day12\input.txt");
+        var result = GardenGroups.FencingCost(input);
+        Assert.Equal(1450816, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(@".\Day12\input.txt");
+        var result = GardenGroups.FencingCost(input);
+        Assert.Equal(0, result);
+    }
+}
