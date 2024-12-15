f73b4f051af2600700a339995cbc78357f3d0643
noname <noname@nomail>
2024-12-15 10:59:59 +0100

    Solve day 15 part 1 aoc 2024

---
+++ AdventOfCode2024/Day15/WarehouseWoes.cs
@@ -0,0 +1,73 @@
+﻿
+namespace AdventOfCode2024.Day15;
+public static class WarehouseWoes
+{
+    public static int SumGPSCoordinates(string input)
+    {
+        var (map, moves) = ParseInput(input);
+        var simulation = moves.Select(x => Move(map, x)).ToArray();
+        var sum = simulation.Last().GetPositions().Sum(GPSCoordinate);
+
+        return sum;
+    }
+
+    private static int GPSCoordinate(Position<char> p)
+    {
+        if(p.Value != 'O') return 0;
+
+        var (x, y) = p;
+
+        var gps = (x * 100) + y;
+
+        return (int)gps;
+    }
+
+    private static char[,] Move(char[,] map, char move)
+    {
+        var robot = map.FindPosition('@') ?? throw new ArgumentNullException();
+
+        var path = move switch
+        {
+            '>' => robot.GetEast(),
+            '<' => robot.GetWest(),
+            'v' => robot.GetSouth(),
+            _ => robot.GetNorth(),
+        };
+
+        var next = path.First();
+
+        switch (next.Value) 
+        {
+            case '.':
+                next.Value = '@';
+                robot.Value = '.';
+                break;
+            case 'O':
+                var spaces = path.SkipWhile(x => x.Value == 'O');
+                var last = spaces.First();
+                if (last.Value != '.') break;
+                next.Value = '@';
+                robot.Value = '.';
+                last.Value = 'O';
+                break;
+        }
+
+        return map;
+    }
+
+    private static (char[,] map, char[] moves) ParseInput(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+
+        var map = lines
+            .TakeWhile(x => x.Length > 0)
+            .ToMultidimensionalArray();
+
+        var moves = lines
+            .SkipWhile(x => x.Length > 0)
+            .SelectMany(x => x)
+            .ToArray();
+
+        return (map, moves);
+    }
+}
---
+++ AdventOfCode2024/Day15/WarehouseWoesTests.cs
@@ -0,0 +1,44 @@
+﻿namespace AdventOfCode2024.Day15;
+
+public sealed class WarehouseWoesTests
+{
+    [Fact]
+    public void Part1_Test1()
+    {
+        var input = File.ReadAllText(@".\Day15\input_test1.txt");
+        var result = WarehouseWoes.SumGPSCoordinates(input);
+        Assert.Equal(2028, result);
+    }
+
+    [Fact]
+    public void Part1_Test2()
+    {
+        var input = File.ReadAllText(@".\Day15\input_test2.txt");
+        var result = WarehouseWoes.SumGPSCoordinates(input);
+        Assert.Equal(10092, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(@".\Day15\input.txt");
+        var result = WarehouseWoes.SumGPSCoordinates(input);
+        Assert.Equal(1442192, result);
+    }
+
+    [Fact]
+    public void Part2_Test()
+    {
+        var input = File.ReadAllText(@".\Day15\input_test.txt");
+        var result = WarehouseWoes.SumGPSCoordinates(input);
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(@".\Day15\input.txt");
+        var result = WarehouseWoes.SumGPSCoordinates(input);
+        Assert.Equal(0, result);
+    }
+}
