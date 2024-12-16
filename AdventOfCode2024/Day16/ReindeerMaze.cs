95d6043dfc3d61568569f13c7255dcda204007ae
noname <noname@nomail>
2024-12-16 18:40:39 +0100

    Solve day 16 aoc 2024

---
+++ AdventOfCode2024/Day16/ReindeerMaze.cs
@@ -0,0 +1,63 @@
+﻿using Pos = IncaTechnologies.Collection.Extensions.Position<char>;
+using Path = System.Collections.Generic.Stack<(IncaTechnologies.Collection.Extensions.Position<char> Pos, IncaTechnologies.Collection.Extensions.Direction Dir)>;
+namespace AdventOfCode2024.Day16;
+public static class ReindeerMaze
+{
+    public static int LowestScore(string input)
+    {
+        var maze = ParseMaze(input);
+        var start = maze.FindPosition('S') ?? throw new Exception("Start not found");
+
+        var path = new Path();
+        path.Push((start, Direction.Right));
+
+        var pq = new PriorityQueue<Path, int>();
+        pq.Enqueue(path, 0);
+
+        while(pq.TryDequeue(out path, out var p))
+        {
+            if (path.Peek() is { Pos.Value: 'E' }) return p;
+
+            var n = Next(path);
+
+            foreach(var (x, sc) in n)
+            {
+                pq.Enqueue(x, p + sc);
+            }
+        }
+
+        throw new Exception("Exit noy found");
+    }
+
+    private static IEnumerable<(Path, int)> Next(Path path)
+    {
+        var (pos, dir) = path.Pop();
+        var positions = path.Select(x => x.Pos).ToArray();
+        var adjacent = pos
+            .GetAdjacent()
+            .Where(x => positions.Contains(x) is false && x is { Value: '.' or 'E'});
+
+        var next = adjacent.Select(a =>
+        {
+            var aDir = Navigation.GetDirection(pos, a);
+            var score = aDir == dir ? 1 : 1001;
+            var newPath = new Path(path);
+            newPath.Push((pos, aDir));
+            newPath.Push((a, aDir));
+
+            return (newPath, score);
+        });
+        
+        return next.ToArray();
+    }
+
+    private static char[,] ParseMaze(string input)
+    {
+        var maze = input
+            .Split(Environment.NewLine)
+            .Select(x => x.ToCharArray())
+            .ToMultidimensionalArray();
+
+        return maze;
+    } 
+}
---
+++ AdventOfCode2024/Day16/ReindeerMazeTests.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2024.Day16;
+
+public sealed class ReindeerMazeTests
+{
+    [Fact]
+    public void Part1_Test1()
+    {
+        var input = File.ReadAllText(@".\Day16\input_test1.txt");
+        var result = ReindeerMaze.LowestScore(input);
+        Assert.Equal(7036, result);
+    }
+
+    [Fact]
+    public void Part1_Test2()
+    {
+        var input = File.ReadAllText(@".\Day16\input_test2.txt");
+        var result = ReindeerMaze.LowestScore(input);
+        Assert.Equal(11048, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(@".\Day16\input.txt");
+        var result = ReindeerMaze.LowestScore(input);
+        Assert.Equal(0, result);
+    }
+}
