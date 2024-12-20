d8d52da3d4c638503123d3e08d502b99cebe02df
noname <noname@nomail>
2024-12-20 10:27:46 +0100

    Solve day 19 aoc 2024

---
+++ AdventOfCode2024/Day19/LinenLayout.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode2024.Day18;
+
+namespace AdventOfCode2024.Day19;
+public static class LinenLayout
+{
+    public static int CountPossibleLayouts(string input)
+    {
+        var (availableLayouts, desiredLayouts) = ParseInput(input);
+        var count = desiredLayouts.Count(x => CountPossibleLayouts(x, availableLayouts, []) > 0);
+        return count;
+    }
+
+    public static long CountAllPossibleLayouts(string input)
+    {
+        var (availableLayouts, desiredLayouts) = ParseInput(input);
+        var count = desiredLayouts.Sum(x => CountPossibleLayouts(x, availableLayouts, []));
+        return count;
+    }
+
+    private static long CountPossibleLayouts(string desiredLayout, string[] availableLayouts, Dictionary<string, long> cache)
+    {
+        if (cache.TryGetValue(desiredLayout, out var cached)) return cached;
+
+        long possible = 0;
+
+        foreach (var availableLayout in availableLayouts)
+        {
+            if (desiredLayout == availableLayout)
+            {
+                possible++;
+                continue;
+            }
+
+            if (availableLayout.Length > desiredLayout.Length) continue;
+
+            var bitDesired = desiredLayout[..(availableLayout.Length)];
+
+            if (bitDesired != availableLayout) continue;
+
+            possible += CountPossibleLayouts(desiredLayout[(bitDesired.Length)..], availableLayouts, cache);
+        }
+
+        cache.TryAdd(desiredLayout, possible);
+
+        return possible;
+    }
+
+    private static (string[] AvailableLayouts, string[] DesiredLayouts) ParseInput(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var availableLayouts = lines[0].Split(',', StringSplitOptions.TrimEntries);
+        var desiredLayouts = lines[2..];
+
+        return (availableLayouts, desiredLayouts);
+    }
+}
---
+++ AdventOfCode2024/Day19/LinenLayoutTests.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2024.Day19;
+
+public sealed class LinenLayoutTests
+{
+    [Fact]
+    public void Part1_Test()
+    {
+        var input = File.ReadAllText(@".\Day19\input_test.txt");
+        var result = LinenLayout.CountPossibleLayouts(input);
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void Part1_Solution()
+    {
+        var input = File.ReadAllText(@".\Day19\input.txt");
+        var result = LinenLayout.CountPossibleLayouts(input);
+        Assert.Equal(251, result);
+    }
+
+    [Fact]
+    public void Part2_Test()
+    {
+        var input = File.ReadAllText(@".\Day19\input_test.txt");
+        var result = LinenLayout.CountAllPossibleLayouts(input);
+        Assert.Equal(16, result);
+    }
+
+    [Fact]
+    public void Part2_Solution()
+    {
+        var input = File.ReadAllText(@".\Day19\input.txt");
+        var result = LinenLayout.CountAllPossibleLayouts(input);
+        Assert.Equal(616957151871345, result);
+    }
+}
