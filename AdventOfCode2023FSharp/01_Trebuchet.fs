aefcd99f7c0de2a90a407970b35b6dee5e6bfb1d
noname <noname@nomail>
2025-02-20 13:57:09 +0100

    Some fsharp for a change

--- AdventOfCode2023FSharp/01_Trebuchet.fs
+++ AdventOfCode2023FSharp/01_Trebuchet.fs
@@ -3,6 +3,19 @@
 module Trebuchet =
 
     open System
+    open System.IO
+
+    let trebuchetCoordinates path = 
+        File.ReadAllLines(path)
+        |> Seq.map (fun line -> int $"{Seq.find Char.IsDigit line}{Seq.findBack Char.IsDigit line}")
+        |> Seq.sum
+
+    let trebuchetCoordinates3 path = 
+        let mutable sum  = 0
+        for line in File.ReadAllLines(path) do
+            sum <- sum + int $"{Seq.find Char.IsDigit line}{Seq.findBack Char.IsDigit line}"
+        sum
+        
 
     let firstDigit (line: string) = line |> Seq.find Char.IsDigit
 
@@ -10,9 +23,9 @@ module Trebuchet =
 
     let trebuchetCoordinate (line: string) = int $"{firstDigit line}{lastDigit line}"
 
-    let getLines (input: string) = input.Split(Environment.NewLine)
+    let getLines (input:  string) = input.Split(Environment.NewLine)
 
-    let trebuchetCoordinates (input: string) = 
+    let trebuchetCoordinates2 (input: string) = 
         input 
         |> getLines 
         |> Seq.map trebuchetCoordinate
--- AdventOfCode2023FSharp/main.fs
+++ AdventOfCode2023FSharp/main.fs
@@ -2,6 +2,10 @@
 
 module main =
 
-    open GearRatios
+    open Trebuchet
+    open System.IO
 
-    printfn "%d" (sumPartNumber input)
+    printfn "%d" (trebuchetCoordinates @"D:\VisualStudio\AdventOfCode\AdventOfCode2023\Day1\Input.txt")
+    printfn "%d" (trebuchetCoordinates3 @"D:\VisualStudio\AdventOfCode\AdventOfCode2023\Day1\Input.txt")
+    printfn "%d" (File.ReadAllText(@"D:\VisualStudio\AdventOfCode\AdventOfCode2023\Day1\Input.txt") |> trebuchetCoordinates2 |> Seq.sum)
+                                      
