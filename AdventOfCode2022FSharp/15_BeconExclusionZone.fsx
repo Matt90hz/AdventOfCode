e0b04c8862756c1687808b3908876d558901b20c
noname <noname@nomail>
2025-11-24 18:59:00 +0100

    2022 day 15 Fsharp

--- AdventOfCode2022FSharp/15_BeconExclusionZone.fsx
+++ AdventOfCode2022FSharp/15_BeconExclusionZone.fsx
@@ -1,103 +1,110 @@
 ﻿open System.IO
 
-let sensorsAndBeacons =
-    @"D:\VisualStudio\AdventOfCode\AdventOfCode2022\Day15\input.txt"
-    //@"D:\VisualStudio\AdventOfCode\AdventOfCode2022\Day15\input_test.txt"
+let sensorsAndBeacons path =
+    path
     |> File.ReadAllLines
-    |> Array.map (fun line -> 
-        let sx = 
-            line 
+    |> Array.map (fun line ->
+        let sx =
+            line
             |> Seq.skipWhile (fun c -> c <> 'x')
             |> Seq.skip 2
-            |> Seq.takeWhile (fun c -> c <> ',') 
-            |> fun digits -> new string(Seq.toArray digits)
+            |> Seq.takeWhile (fun c -> c <> ',')
+            |> fun digits -> new string (Seq.toArray digits)
             |> int
 
-        let sy = 
-            line 
+        let sy =
+            line
             |> Seq.skipWhile (fun c -> c <> 'y')
             |> Seq.skip 2
-            |> Seq.takeWhile (fun c -> c <> ':') 
-            |> fun digits -> new string(Seq.toArray digits)
+            |> Seq.takeWhile (fun c -> c <> ':')
+            |> fun digits -> new string (Seq.toArray digits)
             |> int
 
-        let by = 
-            line 
+        let by =
+            line
             |> Seq.rev
-            |> Seq.takeWhile (fun c -> c <> '=') 
+            |> Seq.takeWhile (fun c -> c <> '=')
             |> Seq.rev
-            |> fun digits -> new string(Seq.toArray digits)
+            |> fun digits -> new string (Seq.toArray digits)
             |> int
 
-        let bx = 
-            line 
+        let bx =
+            line
             |> Seq.rev
             |> Seq.skipWhile (fun c -> c <> ',')
             |> Seq.skip 1
             |> Seq.takeWhile (fun c -> c <> '=')
             |> Seq.rev
-            |> fun digits -> new string(Seq.toArray digits)
+            |> fun digits -> new string (Seq.toArray digits)
             |> int
 
         sx, sy, bx, by)
 
-let rec combineRanges (ranges: (int*int) list)=
-    let combineTwoRange (r: int*int) (r': int*int)=
+let rec combineRanges (ranges: (int * int) list) =
+
+    let combineTwoRange (r: int * int) (r': int * int) =
         match r, r' with
-        | (x, y), (x', y') when y < x' || x > y' -> None
+        | (x, y), (x', y') when y < x' - 1 || x > y' + 1 -> None
         | (x, y), (x', y') -> Some(min x x', max y y')
 
     match ranges with
-    | [] | [_] -> ranges
-    | [r; r'] -> 
-        match (combineTwoRange r r') with 
-        | Some r'' -> [r'']
+    | []
+    | [ _ ] -> ranges
+    | [ r; r' ] ->
+        match (combineTwoRange r r') with
+        | Some r'' -> [ r'' ]
         | None -> ranges
     | r :: x ->
-        let combined = x |> Seq.indexed |> Seq.tryPick (fun (i, r') ->
-            match combineTwoRange r r' with
-            | Some c -> Some(i, c)
-            | None -> None)
+        let combined =
+            x
+            |> Seq.indexed
+            |> Seq.tryPick (fun (i, r') ->
+                match combineTwoRange r r' with
+                | Some c -> Some(i, c)
+                | None -> None)
+
         match combined with
-        | Some (i, c) -> x |> List.removeAt i |> fun x -> combineRanges (c :: x)
+        | Some(i, c) -> x |> List.removeAt i |> (fun x -> combineRanges (c :: x))
         | None -> r :: combineRanges x
 
-let noBeconsPositions=
-    sensorsAndBeacons
-    |> Seq.choose (fun (sx, sy, bx, by) -> 
-        let y = 2_000_000
-        let d = abs(sx - bx) + abs(sy - by)
-        let dy = abs(sy - y) 
-        let dx = abs(d - dy)    
+let noBeconsPositions y path =
+    sensorsAndBeacons path
+    |> Seq.choose (fun (sx, sy, bx, by) ->
+        let d = abs (sx - bx) + abs (sy - by)
+        let dy = abs (sy - y)
+        let dx = abs (d - dy)
         if dy > d then None else Some(sx - dx, sx + dx))
     |> Seq.toList
     |> combineRanges
-    |> List.sumBy (fun (x, y) -> abs(x - y)) // = 4_827_924
+    |> List.sumBy (fun (x, y) -> abs (x - y))
 
-let onlyPossibleBeaconTuningFrequency = 
-    let minCoo, maxCoo = 0, 4_000_000
-    //let minCoo, maxCoo = 0, 20
-    let tuningFrequency (x, y) = int64 x * 4_000_000L + int64 y
 
-    let gap = function
-        | [(x, y); (x', y')] when x < x' -> y + 1 
-        | [(x, y); (x', y')] when x > x' -> y' + 1 
-        | _ -> -69
+let onlyPossibleBeaconTuningFrequency fieldSize path =
+    let minCoo, maxCoo = 0, fieldSize
+    let sb = sensorsAndBeacons path
+
+    let gap ranges =
+        ranges |> Seq.map (fun (_, y) -> y) |> Seq.min
+
+    let tuningFequency (x, y) = int64 x * 4_000_000L + int64 y
 
     seq { minCoo..maxCoo }
-    |> Seq.map (fun x -> 
-        sensorsAndBeacons
-        |> Seq.choose (fun (sx, sy, bx, by) -> 
-            let d = abs(sx - bx) + abs(sy - by)
-            let dx = abs(sx - x) 
-            let dy = abs(d - dx)    
+    |> Seq.pick (fun x ->
+        sb
+        |> Seq.choose (fun (sx, sy, bx, by) ->
+            let d = abs (sx - bx) + abs (sy - by)
+            let dx = abs (sx - x)
+            let dy = abs (d - dx)
             if dx > d then None else Some(sy - dy, sy + dy))
         |> Seq.toList
         |> combineRanges
-        |> fun r -> x, r)
-    |> Seq.find (fun (y,r) -> List.length r > 1)
-    |> fun (x, r) -> x, r, tuningFrequency (x, gap r)
-  
-    // 12977110973564
-    // 3244277 2973564
-       
+        |> fun ranges -> if List.length ranges > 1 then Some(x, gap ranges) else None)
+    |> tuningFequency
+
+let path = @"D:\VisualStudio\AdventOfCode\AdventOfCode2022\Day15\input.txt"
+let testPath = @"D:\VisualStudio\AdventOfCode\AdventOfCode2022\Day15\input_test.txt"
+
+printfn "%i expected %i" (noBeconsPositions 10 testPath) 26
+printfn "%i expected %i" (noBeconsPositions 2_000_000 path) 4_827_924
+printfn "%i expected %i" (onlyPossibleBeaconTuningFrequency 20 testPath) 56000010
+printfn "%i expected %i" (onlyPossibleBeaconTuningFrequency 4_000_000 path) 12977110973564L
