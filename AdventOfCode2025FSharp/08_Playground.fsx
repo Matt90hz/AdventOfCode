563eeb0ca40d212e8ec340e2891b8efbac706b11
noname <noname@nomail>
2025-12-25 11:06:34 +0100

    Aoc 2025 day 8 fsharp

--- AdventOfCode2025FSharp/08_Playground.fsx
+++ AdventOfCode2025FSharp/08_Playground.fsx
@@ -5,12 +5,11 @@ let path = @"D:\VisualStudio\AdventOfCode\AdventOfCode2025\Day08\input.txt"
 
 let coordinates =
     File.ReadAllLines(path)
-    |> Seq.map (fun line ->
-        let coo = line.Split(',')
-        (float coo[0], float coo[1], float coo[2]))
+    |> Seq.map (_.Split(',') >> Array.map float)
+    |> Seq.map (fun coo -> (coo[0], coo[1], coo[2]))
     |> Seq.toList
 
-let circuits = coordinates |> Seq.map (fun jbox -> Set.ofList [ jbox ]) |> Set.ofSeq
+let circuits = coordinates |> Seq.map (Seq.singleton >> Set.ofSeq) |> Set.ofSeq
 
 let dist (x, y, z) (x', y', z') =
     (x - x') ** 2.0 + (y - y') ** 2.0 + (z - z') ** 2.0
@@ -41,8 +40,6 @@ let largestCircuits =
     |> Seq.take 3
     |> Seq.reduce (*) // 80446
 
-#time
-
 let gigaCircuit =
     seq {
         for i in 0 .. (List.length coordinates - 2) do
@@ -51,23 +48,6 @@ let gigaCircuit =
     }
     |> Seq.sortBy ((<||) dist)
     |> Seq.scan
-        (fun (_, circuits) (jb, jb') ->
-            let circuit = Seq.find (Set.contains jb) circuits
-            let circuit' = Seq.find (Set.contains jb') circuits
-
-            if circuit = circuit' then
-                (jb, jb'), circuits
-            else
-                (jb, jb'),
-                circuits
-                |> Set.remove circuit
-                |> Set.remove circuit'
-                |> Set.add (Set.union circuit circuit'))
+        (fun (_, circuits) jboxes -> jboxes, connect circuits jboxes)
         (((0.0, 0.0, 0.0), (0.0, 0.0, 0.0)), circuits)
-    |> Seq.pick (fun (((x, _, _), (x', _, _)), circuits) ->
-        if circuits |> Set.count |> (=) 1 then
-            Some(x * x')
-        else
-            None) // 51294528
-
-#time
+    |> Seq.pick (fun (((x, _, _), (x', _, _)), circuits) -> if Set.count circuits = 1 then Some(x * x') else None) // 51294528
