0eeba52e9942ea43f8a9736fbe8de5d217d6678f
noname <noname@nomail>
2026-01-29 20:55:27 +0100

    day 12 parsing

---
+++ AdventOfCode2025FSharp/12_ChristmasTreeFarm.fsx
@@ -0,0 +1,49 @@
+open System
+open System.IO
+
+let inputPath =
+    match fsi.CommandLineArgs with
+    | [| _; "-t" |] -> @"D:\VisualStudio\AdventOfCode\AdventOfCode2025\Day12\input_test.txt"
+    | _ -> @"D:\VisualStudio\AdventOfCode\AdventOfCode2025\Day12\input.txt"
+
+let presents, regions =
+    let blob =
+        File.ReadAllLines(inputPath)
+        |> Seq.fold
+            (fun blob line ->
+                match blob, line with
+                | [], line -> [ line + "\n" ]
+                | bag :: blob, line when String.IsNullOrWhiteSpace line -> String.Empty :: bag :: blob
+                | bag :: blob, line -> bag + line + "\n" :: blob)
+            []
+
+    let presents = 
+        blob 
+        |> List.tail
+        |> List.map (fun bag -> 
+            let split = bag.Split(':')
+            let code = int split[0]
+            let gift = 
+                split[1].Split('\n', StringSplitOptions.RemoveEmptyEntries)
+                |> Array.map _.ToCharArray()
+                |> fun jagged -> 
+                    Array2D.init jagged.Length jagged[0].Length (fun x y -> jagged[x][y])
+
+            code, gift)
+
+    let regions =
+        blob
+        |> List.head
+        |> _.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun line ->
+            let split = line.Split(':')
+            let size = split[0].Split('x')
+
+            let nums =
+                split[1].Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map int
+
+            int size[0], int size[1], nums)
+
+    presents, regions
+
+printfn "%A" (presents, regions)
